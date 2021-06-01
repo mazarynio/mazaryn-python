e7397300b5c982eb6e6310ffaeabb5f4a7ba0d69
Peter Evance <peterevance1@gmail.com>
2021-06-01 16:58:00 +0300

    Profiles app now serializes register, login etc.

--- mazaryn/urls.py
+++ mazaryn/urls.py
@@ -4,13 +4,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home_view
 from rest_framework.authtoken import views  # for testing purposes
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("profiles/", include("profiles.urls")),
+    path("", include("profiles.urls")),
     path("posts/", include("posts.urls")),
     path("groups/", include("groups.urls")),
     path("token/", views.obtain_auth_token),  # for testing purposes
