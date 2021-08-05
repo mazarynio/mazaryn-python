b770d0f59c562dd2a28f21c716a05c43fda3ed9a
Peter Evance <peterevance1@gmail.com>
2021-08-05 17:08:40 +0300

    Removed posts url

--- mazaryn/urls.py
+++ mazaryn/urls.py
@@ -11,11 +11,9 @@ from rest_framework.authtoken import views  # for testing purposes
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('chat/', include('communications.urls')),
-    path("posts/", include("posts.urls")),
     path("groups/", include("groups.urls")),
     url(r'^auth/', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.authtoken')),
-    # url(r'^auth/', include('djoser.urls.jwt')),
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
