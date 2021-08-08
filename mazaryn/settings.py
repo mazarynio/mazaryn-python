c85a37499a39ecf44a7849abeb8e8083925df5b3
lewiseman <lewisemann@gmail.com>
2021-08-08 17:02:13 +0300

    documented the api

--- mazaryn/settings.py
+++ mazaryn/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- mazaryn/urls.py
+++ mazaryn/urls.py
@@ -7,6 +7,24 @@ from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework.authtoken import views  # for testing purposes
 
+# for documentation
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Mazaryn",
+      default_version='v1',
+      description="Distributed social system",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,6 +32,8 @@ urlpatterns = [
     path("groups/", include("groups.urls")),
     url(r'^auth/', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.authtoken')),
+
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
