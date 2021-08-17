f914dbe4e683d45695f6536efc3c276a35f03803
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-08-17 18:40:02 +0300

    Update routing.py
    added docstring for URL router

--- communications/routing.py
+++ communications/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 
 from .consumers import ChatConsumer
 
+"""Routing to work out what single consumer to give a connection to """
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<friend>\w+)/$', ChatConsumer.as_asgi()),
 ]
