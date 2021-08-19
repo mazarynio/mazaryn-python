52c5abf591eba1acf75b9c9ff064767b24ef9a00
Peter Evance <peterevance1@gmail.com>
2021-08-20 00:28:57 +0300

    Configured the documentation - admin side.

--- mazaryn/urls.py
+++ mazaryn/urls.py
@@ -27,11 +27,12 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('chat/', include('communications.urls')),
-    path("groups/", include("groups.urls")),
-    url(r'^auth/', include('djoser.urls')),
-    url(r'^auth/', include('djoser.urls.authtoken')),
+   path('admin/doc/', include('django.contrib.admindocs.urls')),
+   path('admin/', admin.site.urls),
+   path('chat/', include('communications.urls')),
+   path('groups/', include("groups.urls")),
+   url(r'^auth/', include('djoser.urls')),
+   url(r'^auth/', include('djoser.urls.authtoken')),
 
    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
