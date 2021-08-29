85b1b858ee5ffe03d49b6a1648703f1112a45d4e
Peter Evance <peterevance1@gmail.com>
2021-08-29 23:08:39 +0300

    Deleted staticfiles folder

--- staticfiles/groups/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import *
-from rest_framework import routers
-
-
-router = routers.DefaultRouter()
-
-router.register('', GroupViewSet)
-
-urlpatterns = router.urls
--- staticfiles/staticfiles/groups/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class GroupsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'groups'
-    
-    def ready(self):
-        import groups.signals
