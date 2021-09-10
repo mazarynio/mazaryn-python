e6bf97b36d11240570f7b0a0673016aebaf24c31
Peter Evance <peterevance1@gmail.com>
2021-09-10 23:21:14 +0300

    Removed commented lines

--- posts/models.py
+++ posts/models.py
@@ -2,21 +2,19 @@ from django.core import validators
 import posts
 from django.db import models
 from django.core.validators import FileExtensionValidator
-# from django.template.defaultfilters import slugify
-# from profiles.utils import get_random_code
+from django.utils import timezone
+import datetime
 
 class Post(models.Model):
     '''This class defines particulars of a post; post-related details such as author, 
     likes count alongside other features represnted by the fields.
     Ordering of the post listing is from the most recent post downards as defined in the meta class.'''
     
-    # title = models.CharField(max_length=50, blank=True)
     content = models.TextField() 
     liked = models.ManyToManyField(
         "profiles.Profile", blank=True, related_name='post_likes')
     groups = models.ForeignKey(
         "groups.Group", blank=True, on_delete=models.SET_NULL, null=True, related_name='group_posts')
-    # slug = models.SlugField(max_length=50, unique=True, null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
@@ -40,30 +38,9 @@ class Post(models.Model):
     def comments(self):
         return self.comment_set.all()
     
-    
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.intial_title = self.title
-        
-    # def save(self, *args, **kwargs):
-    #     '''This function overides the default save() method available for post.
-    #     This function generates the slug based on:
-    #     1. If post title is provided then slug will be generated from them.
-    #     2. Does check on the availability of a slug and ensures that the unique slug is not
-    #     changed each and everytime the post is updated.'''
-    #     ex = False
-    #     to_slug = self.slug
-    #     if self.title != self.intial_title or self.slug == "":
-    #         if self.title:
-    #             to_slug = slugify(str(self.title))
-    #             ex = Post.objects.filter(slug=to_slug).exists()
-    #             while ex:
-    #                 to_slug = slugify(to_slug + '-' + str(get_random_code()))
-    #                 ex = Post.objects.filter(slug=to_slug).exists()
-    #         else:
-    #             to_slug = slugify(str(self.user) + '-' + 'post' + str(get_random_code()))
-    #     self.slug = to_slug
-    #     super().save(*args, **kwargs)
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class PostImage(models.Model):
     '''This model handles a particular image(s) of a post and its full functionality is 
